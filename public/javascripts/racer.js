a0c0fbf851252e4d8c4592b53e9e9f983c002290
Nicolas Lopierre Aguirre <nl1412@nyu.edu>
2017-11-29 16:24:04 -0500

    set up the handleclick

--- public/javascripts/racer.js
+++ public/javascripts/racer.js
@@ -1,7 +1,22 @@
 const socket = io();
 
+function handleClickPlayer1(e){
+    e.preventDefault();
+    socket.emit("player1CLick");
+}
+
+function handleClickPlayer2(e){
+    e.preventDefault();
+    socket.emit("player2CLick");
+}
+
 function main(){
-    
+    //create finish line
+    div.document.createElement("div");
+
+    //add event listeners to clicks
+    document.querySelector(".player1Btn").addEventListener("clicked", handleClickPlayer1);
+    document.querySelector(".player2Btn").addEventListener("clicked", handleClickPlayer2);
 }
 
 document.addEventListener("DOMContentLoaded", main);
